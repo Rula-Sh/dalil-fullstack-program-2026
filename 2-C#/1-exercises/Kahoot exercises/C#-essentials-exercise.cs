92e22579da82586c9c7d9662a12045eff42691bf
Rula-Sh <1.rulashaban@gmail.com>
2026-09-23 12:23:40 +0300

    Add Kahoot exerices

---
+++ 2-C#/1-exercises/Kahoot exercises/C#-essentials-exercise.cs
@@ -0,0 +1,48 @@
+/*
+    ====== The exercise took place on https://kahoot.it/ website ======
+    Q1: What does a Value Type store?
+        A) A reference to an object
+        B) Memory Address
+        C) The data itself 
+        D) A pointer to the heap
+
+
+    Q2: Which one is a Reference Type:
+        A) int
+        B) string
+        C) double
+        D) char
+
+
+    Q3: Boxing is usually:
+        A) slow
+        B) explicit
+        C) implicit
+        D) automatic
+
+
+    Q4: What does the & Operator mean when using it with a pointer?
+        A) Bitwise AND
+        B) Reference operator
+        C) Address-of operator
+        D) get the memory address
+
+
+    Q5: What does Unboxing convert?
+        A) int num = (int)obj;
+        B) object obj = 10;
+        C) var var1 = obj;
+        D) None of the above.
+*/
+
+
+
+/*
+    Answers:
+    Q1: C) The data itself
+    Q2: B) string
+    Q3: C) implicit
+    Q4: D) get the memory address
+    Q5: A) int num = (int)obj;
+    "Won in 2nd place"
+*/
---
+++ 2-C#/1-exercises/Kahoot exercises/C#-linq-files-and-solid_principles.cs
@@ -0,0 +1,103 @@
+/*
+    ====== The exercise took place on https://kahoot.it/ website ======
+    Q1: What does FindAll() return?
+        A) The first matching element
+        B) All elements that satisfy the condition
+        C) A Boolean value
+        D) The largest element
+
+
+    Q2: What is the main purpose of Select?
+        A) Delete elements
+        B) Transform each element into another value/form
+        C) Find only one element
+        D) Check if all elements match
+
+
+    Q3: What happens if First() cannot find a matching element
+        A) Returns 0
+        B) Returns null
+        C) Throws an exception
+        D) Returns false
+
+
+    Q4: What is the main difference between First() and Single()?
+        A) First() expects one match, while Single() returns the first match
+        B) First() sorts the list
+        C) Single() always returns the first element
+        D) First() returns the first match, while Single() expects exactly one match
+
+
+    Q5: Which class is mainly used to get information about a file?
+        A) StreamReader
+        B) StreamWriter
+        C) FileInfo
+        D) Console
+
+
+    Q6: Which property gives the file size in bytes?
+        A) Length(Bytes)
+        B) Length
+        C) FullName
+        D) CreationTime
+
+
+    Q7: What does ReadLine() return when the end of the file is reached?
+        A) 0
+        B) false
+        C) null
+        D) An empty file
+
+
+    Q8: What does the S in SOLID stand for?
+        A) Simple Programming
+        B) Single Responsibility Principle
+        C) Static Responsibility
+        D) System Responsibility
+
+
+    Q9: What does Open/Closed Principle encourage?
+        A) Modify existing code every time we add a feature
+        B) Extend behavior without modifying existing
+        C) Avoid using classes
+        D) Use only static methods
+
+
+    Q10: Which SOLID principle says that subclasses should be usable in place of their base classes?
+         A) SRP
+         B) OCP
+         C) LSP
+         D) ISP
+
+
+    Q11: What is the main idea of Interface Segregation Principle?
+         A) Use one huge interface
+         B) Don't force classes to implement methods they don't need
+         C) Use only one interface per project
+         D) Never use interfaces
+
+
+    Q12: What problem does the Strategy Pattern mainly solve?
+         A) It allows different algorithms/behaviors to be switched
+         B) Hide data-access/storage details behind abstraction
+         C) It reads database tables
+         D) Handle UI
+*/
+
+
+
+/*
+    Answers:
+    Q1: B) All elements that satisfy the condition
+    Q2: B) Transform each element into another value/form
+    Q3: C) Throws an exception
+    Q4: D) First() returns the first match, while single expects exactly one match
+    Q5: C) FileInfo
+    Q6: B) Length
+    Q7: C) null
+    Q8: B) Single Responsibility Principle
+    Q9: B) Extend behavior without modifying existing
+    Q10:C) LSP
+    Q11:B) Don't force calsses to implement methods they don't need
+    Q12:A) It allows different algorithms/behaviors to be switched
+*/
